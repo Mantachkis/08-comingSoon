dfdfd9f3fbab0f65aaffcd29b6be330458d76bc7
Mantachkis <82802203+Mantachkis@users.noreply.github.com>
2021-05-17 15:33:10 +0300

    a

--- js/components/progres-bar/progressBar.js
+++ js/components/progres-bar/progressBar.js
@@ -4,11 +4,16 @@ function progressBar(selector, data){
     //logic
     const DOM= document.querySelector(selector);
     let HTML= '';
-    for( const progress of data){
+    for (const progress of data) {
         HTML += `<div class="progress-bar">
-        PROGRESS BAR
-    </div>`
-    }
+                    <div class="top">
+                        <div class="label">${progress.label}</div>
+                        <div class="value">${progress.value}</div>
+                    </div>
+                    <div class="bottom">
+                        <div class="progress"></div>
+                    </div>
+                </div>`;
     
     
     //result return
