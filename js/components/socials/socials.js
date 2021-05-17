bcb7d0473c049522398b405b25a231d3c094370f
Mantachkis <82802203+Mantachkis@users.noreply.github.com>
2021-05-17 11:57:58 +0300

    socials design

---
+++ js/components/socials/socials.js
@@ -0,0 +1,25 @@
+function socials(selector, data){
+    // validacija
+
+    // logika
+    const DOM = document.querySelector(selector);
+
+    let HTML = '';
+
+    
+    for(const social of data){
+        if(social.active){
+        HTML += `<a href="${socials.href}" target="_blank" class="fa fa-${social.icon}"></a>`
+        }
+    }
+
+    // for(const {href,icon} of data){
+    //     HTML += `<a href="${href}" target="_blank" class="fa fa-${icon}"></a>`
+    // }
+
+    //result return
+    DOM.innerHTML = HTML;
+
+}
+
+export { socials }
